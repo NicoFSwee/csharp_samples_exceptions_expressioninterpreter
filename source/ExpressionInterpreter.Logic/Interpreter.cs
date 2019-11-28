21f82fa5fdfab0861fe27d869e7117c4f9ae60be
nicof <nicof@DESKTOP-8K81C9C>
2019-11-28 20:28:28 +0100

    Add some functions, WIP

--- source/ExpressionInterpreter.Logic/Interpreter.cs
+++ source/ExpressionInterpreter.Logic/Interpreter.cs
@@ -94,7 +94,15 @@ namespace ExpressionInterpreter.Logic
         /// </summary>
         public void ParseExpressionStringToFields()
         {
+            for (int i = 0; i < ExpressionText.Length; i++)
+            {
+                SkipBlanks(ref i);
+                _operandRight = ScanNumber(ref i);
+                SkipBlanks(ref i);
+                _operandLeft = ScanNumber(ref i);
+                SkipBlanks(ref i);
 
+            }
         }
 
         /// <summary>
@@ -105,7 +113,89 @@ namespace ExpressionInterpreter.Logic
         /// <returns></returns>
         private double ScanNumber(ref int pos)
         {
-            throw new NotImplementedException();
+            bool isNegative = false;
+            bool containsComma = false;
+            double result = 0;
+
+            if(ExpressionText[pos] == '-')
+            {
+                isNegative = true;
+                pos++;
+                SkipBlanks(ref pos);
+            }
+
+            //Now the pos is at the first digit that occurs.
+            int posTmp = pos;
+            while(ExpressionText[pos] != '+' && ExpressionText[pos] != '-' 
+                && ExpressionText[pos] != '*' && ExpressionText[pos] != '/')
+            {
+                if(ExpressionText[pos] == ',')
+                {
+                    containsComma = true;
+                }
+                pos++;
+            }
+            _op = ExpressionText[pos];
+
+            //Now we got the Operator and reset the pos to the position of the first digit.
+            pos = posTmp;
+
+            int faktor = 1;
+            //Do we have a comma?
+            if (!containsComma)
+            {
+                while(ExpressionText[pos] != _op || !char.IsWhiteSpace(ExpressionText[pos]))
+                {
+                    pos++;  //Count the positions until the digit ends.
+                }
+
+                //Now we are at the next pos after the last digit and we look at the digits beginning from the end
+                for (int i = pos - 1; i >= posTmp; i++)
+                {
+                    int number = ScanInteger(ref i);
+                    result += number * faktor;
+                    faktor *= 10;
+                }
+                //Now we got the number on the left side
+            }
+            else
+            {
+                //we got a comma, what now?
+
+                while(ExpressionText[pos] != ',')
+                {
+                    pos++;
+                }
+                //Move to the pos of the comma.
+
+                for (int i = pos - 1; i > posTmp; i++)
+                {
+                    int number = ScanInteger(ref i);
+                    result += number * faktor;
+                    faktor *= 10;
+                }
+
+
+                //now we should have the number on the left side of the comma;
+                pos++; //Move the pos on the next digit, away from the comma.
+                posTmp = pos;
+                while (char.IsDigit(ExpressionText[pos]))
+                {
+                    pos++;
+                }
+
+                //Now we are behind the last digit of the left side.
+
+                for (int i = posTmp; i < pos; i++)
+                {
+                    int number = ScanInteger(ref i);
+                    result += number / faktor;
+                    faktor *= 10;
+                }
+
+            }
+
+            return result;
         }
 
         /// <summary>
@@ -115,7 +205,11 @@ namespace ExpressionInterpreter.Logic
         /// <returns></returns>
         private int ScanInteger(ref int pos)
         {
-            throw new NotImplementedException();
+            int number;
+
+            number = ExpressionText[pos] - '0';
+
+            return number;
         }
 
         /// <summary>
@@ -124,7 +218,10 @@ namespace ExpressionInterpreter.Logic
         /// <param name="pos"></param>
         private void SkipBlanks(ref int pos)
         {
-            throw new NotImplementedException();
+            while(char.IsWhiteSpace(ExpressionText[pos]))
+            {
+                pos++;
+            }
         }
 
         /// <summary>
