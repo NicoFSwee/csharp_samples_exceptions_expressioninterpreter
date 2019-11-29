81a2f9d86ab970a6a39b68682dff7086e98c2a1d
NicoFSwee <56690947+NicoFSwee@users.noreply.github.com>
2019-11-29 01:32:05 +0100

    Implement all functions, 2 tests red

--- source/ExpressionInterpreter.Logic/Interpreter.cs
+++ source/ExpressionInterpreter.Logic/Interpreter.cs
@@ -7,7 +7,7 @@ namespace ExpressionInterpreter.Logic
     {
         private double _operandLeft;
         private double _operandRight;
-        private char _op;  // Operator                  
+        private char _op;  // Operator   
 
         /// <summary>
         /// Eingelesener Text
@@ -43,6 +43,140 @@ namespace ExpressionInterpreter.Logic
             }
         }
 
+        private void CheckStringForExceptions(int pos)
+        {
+            if(ExpressionText[pos] == '-')
+            {
+                pos++;
+            }
+            while(pos < ExpressionText.Length - 1 && ExpressionText[pos] != '+' && ExpressionText[pos] != '*' && ExpressionText[pos] != '/' && ExpressionText[pos] != '-')
+            {
+                pos++;
+                if(!char.IsDigit(ExpressionText[pos]) && !char.IsWhiteSpace(ExpressionText[pos])  //Checking for symbols that are not allowed.
+                    && ExpressionText[pos] != '+' && ExpressionText[pos] != '*' 
+                    && ExpressionText[pos] != '/' && ExpressionText[pos] != '-' && ExpressionText[pos] != ',')
+                {
+                    throw new InvalidOperationException($"Operator {ExpressionText[pos]} ist fehlerhaft!");
+                }
+            }
+
+            try
+            {
+                bool leftContainsDigit = false;
+                int leftCommaPos = 0;
+                bool leftHasComma = false;
+                for (int j = 0; j < pos; j++)
+                {
+                    if (char.IsDigit(ExpressionText[j]))
+                    {
+                        leftContainsDigit = true;
+                    }
+                    if (ExpressionText[j] == ',')
+                    {
+                        leftCommaPos = j;
+                        leftHasComma = true;
+                    }
+                }
+                if (!leftContainsDigit)
+                {
+                    throw new ArgumentException("Zahl fehlt komplett");
+                }
+
+                //Now lets check if both sides of the comma have numbers.
+                bool lDigitBeforeComma = false;
+                bool lDigitAfterComma = false;
+                if (leftHasComma)
+                {
+                    for (int k = 0; k < leftCommaPos; k++)
+                    {
+                        if (char.IsDigit(ExpressionText[k]))
+                        {
+                            lDigitBeforeComma = true;
+                        }
+                    }
+                    for (int k = leftCommaPos; k < pos; k++)
+                    {
+                        if (char.IsDigit(ExpressionText[k]))
+                        {
+                            lDigitAfterComma = true;
+                        }
+                    }
+                }
+                if(!lDigitBeforeComma)
+                {
+                    throw new ArgumentException("Ganzzahlanteil ist fehlerhaft");
+                }
+                if (!lDigitAfterComma)
+                {
+                    throw new ArgumentException("Nachkommaanteil ist fehlerhaft");
+                }
+            }
+            catch(ArgumentException e)
+            {
+                throw new ArgumentException("Linker Operand ist fehlerhaft", e);
+            } 
+
+            //Right side.
+            try
+            {
+
+                bool rightContainsDigit = false;
+                int rightCommaPos = 0;
+                bool rightHasComma = false;
+                
+                for (int j = pos; j < ExpressionText.Length; j++)
+                {
+                    if (char.IsDigit(ExpressionText[j]))
+                    {
+                        rightContainsDigit = true;
+                    }
+                    if (ExpressionText[j] == ',')
+                    {
+                        rightCommaPos = j;
+                        rightHasComma = true;
+                    }
+                }
+                if(rightContainsDigit == false)
+                {
+                    throw new ArgumentException("Zahl fehlt komplett");
+                }
+
+                bool rDigitBeforeComma = false;
+                bool rDigitAfterComma = false;
+
+                if (rightHasComma)
+                {
+                    for (int k = pos; k < rightCommaPos; k++)
+                    {
+                        if (char.IsDigit(ExpressionText[k]))
+                        {
+                            rDigitBeforeComma = true;
+                        }
+                    }
+                    for (int k = rightCommaPos; k < ExpressionText.Length; k++)
+                    {
+                        if (char.IsDigit(ExpressionText[k]))
+                        {
+                            rDigitAfterComma = true;
+                        }
+                    }
+                }
+                
+                if(!rDigitBeforeComma)
+                {
+                    throw new ArgumentException("Ganzzahlanteil ist fehlerhaft");
+                }
+                if (!rDigitAfterComma)
+                {
+                    throw new ArgumentException("Nachkommaanteil ist fehlerhaft");
+                }
+            }
+            catch(ArgumentException e)
+            {
+                throw new ArgumentException("Rechter Operand ist fehlerhaft", e);
+            }
+        }
+
         /// <summary>
         /// Wertet den Ausdruck aus und gibt das Ergebnis zurück.
         /// Fehlerhafte Operatoren und Division durch 0 werden über Exceptions zurückgemeldet
@@ -71,7 +205,7 @@ namespace ExpressionInterpreter.Logic
                 }
                 else
                 {
-                    throw new InvalidOperationException();
+                    throw new InvalidOperationException("Division durch 0 ist nicht erlaubt");
                 }
             }
             else
@@ -97,11 +231,21 @@ namespace ExpressionInterpreter.Logic
             for (int i = 0; i < ExpressionText.Length; i++)
             {
                 SkipBlanks(ref i);
-                _operandRight = ScanNumber(ref i);
-                SkipBlanks(ref i);
+                CheckStringForExceptions(i);
                 _operandLeft = ScanNumber(ref i);
                 SkipBlanks(ref i);
+                SkipOperator(ref i);
+                SkipBlanks(ref i);
+                _operandRight = ScanNumber(ref i);
+                SkipBlanks(ref i);
+            }
+        }
 
+        private void SkipOperator(ref int pos)
+        {
+            if(ExpressionText[pos] == _op)
+            {
+                pos++;
             }
         }
 
@@ -117,7 +261,7 @@ namespace ExpressionInterpreter.Logic
             bool containsComma = false;
             double result = 0;
 
-            if(ExpressionText[pos] == '-')
+            if(pos < ExpressionText.Length - 1 && ExpressionText[pos] == '-')
             {
                 isNegative = true;
                 pos++;
@@ -126,8 +270,9 @@ namespace ExpressionInterpreter.Logic
 
             //Now the pos is at the first digit that occurs.
             int posTmp = pos;
-            while(ExpressionText[pos] != '+' && ExpressionText[pos] != '-' 
-                && ExpressionText[pos] != '*' && ExpressionText[pos] != '/')
+            while(pos < ExpressionText.Length - 1 &&  ExpressionText[pos] != '-'      //Loop until we find the operator or reach the end of the string.
+                && ExpressionText[pos] != '*' && ExpressionText[pos] != '/' 
+                && ExpressionText[pos] != '+' )
             {
                 if(ExpressionText[pos] == ',')
                 {
@@ -135,25 +280,33 @@ namespace ExpressionInterpreter.Logic
                 }
                 pos++;
             }
-            _op = ExpressionText[pos];
 
             //Now we got the Operator and reset the pos to the position of the first digit.
+            if(_operandLeft == 0)
+            {
+                _op = ExpressionText[pos];
+            }
+            
             pos = posTmp;
 
-            int faktor = 1;
-            //Do we have a comma?
-            if (!containsComma)
+            double faktor = 1; //Scales with the position of the digit it gets multiplied / divided with.
+            
+            if (!containsComma) 
             {
-                while(ExpressionText[pos] != _op || !char.IsWhiteSpace(ExpressionText[pos]))
+                //Do we have a comma?
+
+                while ((pos < ExpressionText.Length - 1 && ExpressionText[pos] != _op) 
+                    || (pos < ExpressionText.Length - 1 && !char.IsWhiteSpace(ExpressionText[pos])))    //Count the positions until the digit ends.
                 {
-                    pos++;  //Count the positions until the digit ends.
+                    pos++;  
                 }
 
-                //Now we are at the next pos after the last digit and we look at the digits beginning from the end
-                for (int i = pos - 1; i >= posTmp; i++)
+                //Now we are at the next pos after the last digit and we look at the digits beginning from the end, multiplying each digit with its corresponding faktor (1, 10, 100, ...)
+                //and add it to the result
+                for (int i = pos - 1; i >= posTmp; i--)
                 {
                     int number = ScanInteger(ref i);
-                    result += number * faktor;
+                    result += (double)number * faktor;
                     faktor *= 10;
                 }
                 //Now we got the number on the left side
@@ -168,34 +321,42 @@ namespace ExpressionInterpreter.Logic
                 }
                 //Move to the pos of the comma.
 
-                for (int i = pos - 1; i > posTmp; i++)
+                for (int i = pos - 1; i >= posTmp; i--) //Same as above for the left side of the comma.
                 {
                     int number = ScanInteger(ref i);
-                    result += number * faktor;
+                    result += (double)number * faktor;
                     faktor *= 10;
                 }
-
+                faktor = 1;
 
                 //now we should have the number on the left side of the comma;
                 pos++; //Move the pos on the next digit, away from the comma.
                 posTmp = pos;
-                while (char.IsDigit(ExpressionText[pos]))
+                while (ExpressionText.Length - 1 != pos && char.IsDigit(ExpressionText[pos]))
                 {
                     pos++;
                 }
 
-                //Now we are behind the last digit of the left side.
+                //Now we are at the last digit of the left side.
 
                 for (int i = posTmp; i < pos; i++)
                 {
                     int number = ScanInteger(ref i);
-                    result += number / faktor;
-                    faktor *= 10;
+                    faktor *= 10;                           //First digit after comma is a tenth so we increase it before we do stuff.
+                    result += (double)number / faktor;
                 }
 
             }
 
-            return result;
+            if(isNegative)
+            {
+                return result * -1;
+            }
+            else
+            {
+                return result;
+            }
+            
         }
 
         /// <summary>
@@ -218,7 +379,7 @@ namespace ExpressionInterpreter.Logic
         /// <param name="pos"></param>
         private void SkipBlanks(ref int pos)
         {
-            while(char.IsWhiteSpace(ExpressionText[pos]))
+            while(pos < ExpressionText.Length - 1 && char.IsWhiteSpace(ExpressionText[pos]))
             {
                 pos++;
             }
@@ -231,7 +392,23 @@ namespace ExpressionInterpreter.Logic
         /// <returns></returns>
         public static string GetExceptionTextWithInnerExceptions(Exception ex)
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append($"Exceptionmessage: {ex.Message}\r\n");
+            int i = 1;
+
+            while (ex != null)
+            {
+                if(ex.InnerException != null)
+                {
+                    sb.Append($"Inner Exception {i}: {ex.InnerException.Message}\r\n");
+                }
+                i++;
+                ex = ex.InnerException;
+            }
+
+
+            return sb.ToString();
         }
     }
 }
